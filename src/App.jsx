dbeeb2c8b5f2cbae8e1d7745cfef1447b67715c0
Taras Pidkuimukha <taraspidkuimukha@gmail.com>
2026-04-06 18:40:09 +0200

    feat: the counter is created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react';
 import './App.scss';
 
 export const App = () => {
-  const [count] = useState(0);
+  const [count, setCount] = useState(0);
 
   const addOne = () => {
-    // write code here
+    setCount(cur => {
+      return (cur + 1);
+    });
   };
 
   const add100 = () => {
+    setCount(cur => {
+      return (cur + 100);
+    });
     // write code here
   };
 
